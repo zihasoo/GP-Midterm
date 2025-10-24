e3ce78439fd12766612026e4947f632f126565c3
zihasoo <dlquddbs1234@gmail.com>
2025-10-25 00:59:06 +0900

    플레이어 캐릭터 2개 구현 1. 팽귄 캐릭터 - 슬라이드 스킬 2. 기사 캐릭터 - 방패 스킬
    Player 클래스만 상속 받으면 기본적인건 다 돼있어서 빠른 개발 가능
    2개 캐릭터 더 만들고 맵 만들고 네트워크 프로그래밍 시작 해야함

---
+++ Assets/Scripts/PlayerPenguin.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerPenguin : Player
+{
+    public float slideDuration;
+    public float slideCoolTime;
+
+    bool isSliding;
+    bool readySlide;
+    IEnumerator slidingRoutine;
+
+    private new void Start()
+    {
+        base.Start();
+        isSliding = false;
+        readySlide = true;
+        animeParameters.Add("Slide");
+    }
+
+    private void Update()
+    {
+        if (isSliding) return;
+        float moveInput = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveInput = -1f;
+            spRenderer.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1f;
+            spRenderer.flipX = false;
+        }
+        float vx = moveInput * moveSpeed;
+        float vy = rb.velocityY;
+
+        bool groundTest = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer) && rb.velocityY <= 1e-4;
+        if (!isGrounded && groundTest) //처음으로 땅을 만났을 때
+        {
+            isGrounded = true;
+            isFalling = false;
+        }
+        if (!groundTest) isGrounded = false;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && readySlide && moveInput != 0)
+        {
+            vx *= 2f;
+            rb.velocity = new Vector2(vx, vy);
+            isSliding = true;
+            readySlide = false;
+            animator.SetTrigger("StartSlide");
+            SetParameterOnlyTrue("Slide");
+            slidingRoutine = SlidingRoutine();
+            StartCoroutine(slidingRoutine);
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            vy = jumpForce;
+            SetParameterOnlyTrue("Jump");
+        }
+
+        if (!isFalling && !isGrounded && vy < 0)
+        { //처음으로 떨어지기 시작할 때
+            isFalling = true;
+            SetParameterOnlyTrue("Falling");
+        }
+
+        rb.velocity = new Vector2(vx, vy);
+
+        if (!isGrounded) return;
+
+        if (moveInput != 0)
+        {
+            SetParameterOnlyTrue("Run");
+        }
+        else
+        {
+            SetParameterOnlyTrue("IDLE");
+        }
+    }
+
+    IEnumerator SlidingRoutine()
+    {
+        yield return new WaitForSeconds(slideDuration * 0.7f);
+        rb.velocityX = rb.velocity.x * 0.3f;
+        yield return new WaitForSeconds(slideDuration * 0.3f);
+        isSliding = false;
+        animator.SetBool("Slide", false);
+        StartCoroutine(SlideCoolDown());
+    }
+
+    IEnumerator SlideCoolDown()
+    {
+        yield return new WaitForSeconds(slideCoolTime);
+        readySlide = true;
+    }
+
+    protected override void HitInterrupt()
+    {
+        if (slidingRoutine != null)
+            StopCoroutine(slidingRoutine);
+        isSliding = false;
+        StartCoroutine(SlideCoolDown());
+    }
+}
